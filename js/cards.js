bad40d29a69436caebf31de5f0702a2e36a33c28
BillStephens2022 <two4onebill@yahoo.com>
2024-01-17 13:24:04 -0500

    added expandable card on click

---
+++ js/cards.js
@@ -0,0 +1,6 @@
+let card14 = document.getElementById("card14");
+let toggle14 = document.querySelector(".toggle14");
+
+toggle14.addEventListener("click", () => {
+    card14.classList.toggle('active');
+})
