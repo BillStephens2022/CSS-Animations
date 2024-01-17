b4393e8b0e892843d6e5d521a986c8576e8273fd
BillStephens2022 <two4onebill@yahoo.com>
2024-01-17 13:40:24 -0500

    added colorful flickering text using javascript

--- js/text.js
+++ js/text.js
@@ -1,7 +1,15 @@
 let text17 = document.querySelector("#text17");
-let spans = text17.innerHTML.split("").map((letter, i) => {
+let spans17 = text17.innerHTML.split("").map((letter, i) => {
     return `<span style="transition-delay: ${i*40}ms;
     filter:hue-rotate(${i*30}deg)">${letter}</span>`
 }).join('');
 
-text17.innerHTML = spans;
+text17.innerHTML = spans17;
+
+let text18 = document.querySelector("#text18");
+let spans18 = text18.innerText.split("").map((letter, i) => {
+    return `<span style="animation-duration: ${Math.random() * 5}s;
+    filter: hue-rotate(${i*50}deg)">${letter}</span>`
+}).join('');
+
+text18.innerHTML = spans18;
