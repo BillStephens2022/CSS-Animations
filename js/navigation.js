4611bf29e98a0cda9fc60e2ee5aa5ac753c8c7a3
BillStephens2022 <two4onebill@yahoo.com>
2024-01-18 11:29:24 -0500

    added a circular navigation where you click on a toggle menu button and icons appear in a circle

---
+++ js/navigation.js
@@ -0,0 +1,6 @@
+let wrapper = document.querySelector(".menu16-wrapper");
+let toggle = document.querySelector("#toggle16");
+
+toggle.addEventListener("click", () => {
+    wrapper.classList.toggle("active");
+});
