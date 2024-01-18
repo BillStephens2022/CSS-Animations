7801f464a77d3d7e7ddd77d77cac6cb24810dbdf
BillStephens2022 <two4onebill@yahoo.com>
2024-01-18 12:34:26 -0500

    added a tab navigation menu which expands when you click on the toggler

--- js/navigation.js
+++ js/navigation.js
@@ -4,3 +4,11 @@ let toggle = document.querySelector("#toggle16");
 toggle.addEventListener("click", () => {
     wrapper.classList.toggle("active");
 });
+
+
+let toggle17 = document.querySelector("#toggle17");
+
+toggle17.addEventListener("click", () => {
+    toggle17.classList.toggle("active");
+});
+
